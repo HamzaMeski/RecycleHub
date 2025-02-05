314f07bc993e1ba05756b76b06c6ccda161a0bda
HamzaMeski <meskihamza5@gmail.com>
2025-02-05 11:21:49 +0100

    backend starter

---
+++ backend/src/main/java/com/recyclehub/backend/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.recyclehub.backend.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+} 
