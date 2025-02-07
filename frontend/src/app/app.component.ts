aab67af43dc1726df7703154bf4f8e13a29d45c5
HamzaMeski <meskihamza5@gmail.com>
2025-02-07 09:33:52 +0100

    tailwind setup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {PointComponent} from './features/points/point.component';
-import {Point2Component, Point2Module} from './features/points/point2.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    PointComponent,
-    Point2Module
+    RouterOutlet
   ],
   templateUrl: './app.component.html'
 })
